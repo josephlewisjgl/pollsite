552467ba2ef9c49cddd7af1215c3abfe8e33351f
josephlewisjgl <josephgeorgelewis2000@gmail.com>
2021-06-28 16:23:38 +0100

    Add hello world index

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpRequests
+
+def index(response):
+    return HttpResponse("Hello, world. You're at the polls index.")
+    
