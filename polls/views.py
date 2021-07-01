b2903d8963591ec7a896d11e3958533e67b7608f
josephlewisjgl <josephgeorgelewis2000@gmail.com>
2021-07-01 18:29:11 +0100

    Add templates and update views to use shortcuts

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
-def index(response):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list' : latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
     
 
-def details(response, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, 'polls/details.html', context)
 
 def results(response, question_id):
     response = "You're looking at the results of question %s."
