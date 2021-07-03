753e4f707684b1fa81562ce401f95ca027ec1376
josephlewisjgl <josephgeorgelewis2000@gmail.com>
2021-07-03 14:51:24 +0100

    Refactor view functions into classes in views.py and urls.py

--- polls/views.py
+++ polls/views.py
@@ -6,27 +6,22 @@ from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    context = {
-        'latest_question_list' : latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
     
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
 
-def details(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question' : question}
-    return render(request, 'polls/details.html', context)
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question' : question
-    }
-    return render(request, 'polls/results.html', context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
