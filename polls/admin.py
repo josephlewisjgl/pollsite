ff0ec599856e0ca79e42de2a10d061eccf6c8c31
josephlewisjgl <josephgeorgelewis2000@gmail.com>
2021-07-07 21:53:37 +0100

    Update the change question and update question admin pages

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -10,6 +10,10 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields' :['question_text']}), ('Date Information', {'fields' : ['pub_date'], 'classes': ['collapse']})]
 
     inlines = [ChoiceInline]
+
+    list_display = ['question_text', 'pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
 
 class Question(models.Model) :
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')
+
+    @admin.display(
+        boolean = True,
+        ordering = pub_date,
+        description = 'Was published recently?'
+    )
+
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        
     def __str__(self):
         return self.question_text 
+
+    
 
 class Choice(models.Model) :
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
